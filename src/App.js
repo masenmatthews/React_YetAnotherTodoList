0088f2ebf45e67835c8954d797fc32a6930df011
Masen Matthews <masenmatthews@gmail.com>
2019-02-21 11:57:31 -0800

    add ability to add and delete todo items, add uuid package, refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -7,29 +10,60 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: "Take out the trash",
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: "Feed dogs",
         completed: true
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: "Finish homework",
         completed: false
       }
     ]
   }
 
+  // toggle complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  // delete todo
+  // ... operator basically copies everything that is already there. Filter basically says that we want filter out the task that we're deleting
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+
+  // add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      // title translates to title: title because key and value are the same per ES6
+      title,
+      completed: false
+    }
+
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+
 
   render() {
-    console.log(this.state.todos)
     return (
-      <div className="App">
-        <Todos todos={this.state.todos} />
+      <div className="container">
+        <div className="App">
+          <Header addTodo={this.addTodo} />
+          <AddTodo />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -14,17 +14,20 @@ class TodoItem extends Component {
 
   // e brings in event perameter
   // arrow function eliminates the need to implement binding on checkbox input
-  
+
   markComplete = (e) => {
     console.log(this.props)
   }
 
   render() {
+    // you're effectively passing the id and title props up to app.js here.
+    const { id, title } = this.props.todo;
     return (
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox" onChange={this.markComplete.bind(this)} /> {' '}
-          { this.props.todo.title }
+          <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+          { title }
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
         </p>
       </div>
     )
@@ -34,6 +37,16 @@ class TodoItem extends Component {
 // PropTypes
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 9px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
 }
 
 export default TodoItem;
